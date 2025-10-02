75893557ecd5856214542017c221cc7bdfc1b9d1
MarcelloLuciani <marcello.luciani2@gmail.com>
2025-10-03 00:49:35 +0200

    Aggiunta la conversione dei dati da decimale a linguaggio ASP.

--- Scripts/daEXCELaASP/readExcel.py
+++ Scripts/daEXCELaASP/readExcel.py
@@ -1,93 +1,95 @@
 import win32com.client
 import pythoncom
-
 from pathlib import Path
 from sys import exit
 from datetime import date
-
-    #
-    # Per ogni Foglio devo creare il codice corrispondente in formato clingo e salvarlo su un file di uscita:
-    # xls_output(nomeFoglio).
-    # xls_output_param(nomeFoglio, "Parametro", valore). 
-    #
-    # Esempio:
-    # xls_output("pazienti").
-    # xls_output_param("pazienti", .... ) -> paziente(nome, eta, sesso).
-    #
+from decimal import Decimal, ROUND_HALF_UP
 
 try:
-
     pythoncom.CoInitialize()
 
-    # Percorso della cartella Documenti > DoctorPlan
     cartella = Path.home() / "Documents" / "EasyTab"
     cartella.mkdir(parents=True, exist_ok=True)
 
     excel = win32com.client.GetActiveObject("Excel.Application")
     wb = excel.ActiveWorkbook
 
-    lista = []
+    lista_fogli = []
 
-    # Recupero i nomi di tutti i fogli disponibili nel Foglio Excel
     for foglio in wb.Sheets:
-        # Ignoro eventuali fogli nascosti
-        if foglio.Visible == -1:
-            print(foglio.Name)
-            # Non aggiungo fogli che nel nome contengono "Foglio"
-            if "Foglio" not in foglio.Name:
-                lista.append(foglio.name)
+        if foglio.Visible == -1 and "Foglio" not in foglio.Name:
+            lista_fogli.append((foglio.Name).lower())
 
-    
-    for chiave in lista:
-        
-        foglio_selezionato = wb.Sheets(str(chiave))
-        
-        percorso_completo_file = str(cartella) + "\\" + str(chiave) + ".lp"
+    for nome_foglio in lista_fogli:
+        foglio_selezionato = wb.Sheets(nome_foglio)
+        percorso_file = str(cartella / f"{nome_foglio}.lp")
 
-        with open(percorso_completo_file, "w") as f:
-
-            f.write("xls_output(\"" + str(chiave) + "\").\n\n")
+        with open(percorso_file, "w") as f:
+            f.write(f'xls_output("{nome_foglio}").\n\n')
 
             for tabella in foglio_selezionato.ListObjects:
                 intervallo = tabella.Range
                 dati = intervallo.Value
-                lista_dati = [list(riga) for riga in dati]
-                
-                intestazione = lista_dati[0]
-                
-                stringa_commento = "% " + str(chiave) + "( " + ", ".join(str(v) for v in intestazione) + " ).\n\n"
+                listaDati = [list(riga) for riga in dati]
 
+                intestazione = listaDati[0]
+                stringa_commento = "% " + str(nome_foglio) + "( " + ", ".join(str(v) for v in intestazione) + " ).\n\n"
                 f.write(stringa_commento)
 
-                for riga in lista_dati[1:]:
+                colonneDecimali = []
+                for indice, col in enumerate(tabella.ListColumns, start=1):
+                    try:
+                        if col.DataBodyRange.Validation.Type == 2:  # xlValidateDecimal
+                            colonneDecimali.append(indice - 1)  
+                    except:
+                        pass
+                
+                fattoriColonna = {}
+                for indice in colonneDecimali:
+                    max_dec = 0
                     
-                    stringa = str(chiave) + "( "
+                    for riga in listaDati[1:]:
+                        if riga[indice] is not None:
+                            valore = Decimal(str(riga[indice]))
+                            
+                            # Calcolo delle cifre decimali
+                            if valore % 1 != 0:
+                                numDecimali = -(valore.as_tuple().exponent)
+                                if numDecimali > max_dec:
+                                    max_dec = numDecimali
+                    
+                    # Determino il fattore di conversione
+                    if max_dec > 0:
+                        fattoriColonna[indice] = 10 ** max_dec
+                    else:
+                        fattoriColonna[indice] = 1
 
+                # Scrivo i dati formattati
+                for riga in listaDati[1:]:
                     if all(cell is None for cell in riga):
-                        continue  # Salta righe completamente vuote
+                        continue
 
                     valoriFormattati = []
-
-                    for valore in riga:
+                    for indice, valore in enumerate(riga):
                         if valore is None:
                             valoriFormattati.append("none")
-                        elif isinstance(valore, float):
-                            if valore.is_integer():
-                                valoriFormattati.append(str(int(valore)))
-                            else:
-                                valoriFormattati.append(str(valore).replace(",", "."))
-                        elif isinstance(valore, date):
-                            # converto solo la parte data in formato ISO YYYY-MM-DD
-                            data = "\"" + valore.strftime("%Y-%m-%d") + "\""
-                            valoriFormattati.append(data)
-                        else: 
+                        elif isinstance(valore, (float, Decimal)):
+                            val = Decimal(str(valore))
+                            if indice in fattoriColonna:
+                                val *= fattoriColonna[indice]
+                            intero_val = int(val.to_integral_value(rounding=ROUND_HALF_UP))
+                            valoriFormattati.append(str(intero_val))
+                        else:
                             valoriFormattati.append(f'"{valore}"')
 
-                    stringa = f'{chiave}(' + ", ".join(valoriFormattati) + ').'
+                    stringa = f'{nome_foglio}({", ".join(valoriFormattati)}).'
+                    f.write(stringa + "\n")
 
-                    f.write(stringa)
-                    f.write("\n")
-                    
+                # Commenti sui fattori di conversione
+                for indice, fattore in fattoriColonna.items():
+                    f.write(f"% Colonna {intestazione[indice]} (campo {indice+1}): fattore di conversione {fattore}\n")
+                f.write("\n")
+
     pythoncom.CoUninitialize()
     print("File Excel convertito correttamente!")
     print("I file sono disponibili nella cartella Documenti.")
@@ -95,12 +97,8 @@ try:
     exit(0)
 
 except Exception as ex:
-    
     pythoncom.CoUninitialize()
-
-    template = f"Eccezione di tipo {type(ex).__name__}. Argomenti: {ex.args}."
-    print(template) 
-    print("Premere invio per continuare...")
-    input()
+    print(f"Eccezione di tipo {type(ex).__name__}. Argomenti: {ex.args}.")
+    input("Premere invio per continuare...")
     exit(99)
-    
+
